d6e3803be1953e45656b8dc07e7fd055bcd581af
marina-novais <80916020+marina-novais@users.noreply.github.com>
2021-06-09 19:50:12 -0300

    Create quiz-145-11-1-07-A.js

---
+++ front-end/quiz-145-11-1-07-A.js
@@ -0,0 +1,10 @@
+const task = (value, value2) => {
+  return(
+    <ol className="taskList">
+      <li>{value}</li>
+    </ol>
+    <ol className="taskList2">
+      <li>{value2}</li>
+    </ol>
+  )
+}
