0670c772801cd831e40a42b060e7ca1d0ccbd9ff
marina-novais <80916020+marina-novais@users.noreply.github.com>
2021-06-14 11:44:26 -0300

    Create quiz-129-7-2-10-C.js

---
+++ fundamentos/quiz-129-7-2/quiz-129-7-2-10-C.js
@@ -0,0 +1,9 @@
+{
+  name: 'Blind Guardian',
+  formedIn: 1986,
+  lastAlbum: 'Twilight Orchestra: Legacy of the Dark Lands',
+  info: { 
+    genre: 'Power Metal',
+    lastAlbum: 'Beyond the Red Mirror'
+  }
+}
